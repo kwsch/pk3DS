ae7fc6117ea712f3a9df62768ff083cf66668b6e
Kurt <kaphotics@gmail.com>
2015-07-24 20:19:53 -0700

    Add Move Writing & Item R/W objects

--- pk3DS/Structs.cs
+++ pk3DS/Structs.cs
@@ -306,10 +306,12 @@ namespace pk3DS
     public class Move
     {
         public byte Type, Quality, Category, Power, Accuracy, PP, Priority, InflictPercent,
-            HitMin, HitMax, TurnMin, TurnMax, CritStage, Flinch, Recoil, Heal, Targeting,
+            HitMin, HitMax, TurnMin, TurnMax, CritStage, Flinch, Recoil, Targeting,
             Stat1, Stat2, Stat3,
             Stat1Stage, Stat2Stage, Stat3Stage,
             Stat1Percent, Stat2Percent, Stat3Percent;
+
+        public Heal Healing;
         public ushort Inflict, Effect;
         public byte _0xB, _0x1E, _0x1F, _0x20, _0x21;
         public Move(byte[] data)
@@ -332,7 +334,7 @@ namespace pk3DS
             Flinch = data[0xF];
             Effect = BitConverter.ToUInt16(data, 0x10);
             Recoil = data[0x12];
-            Heal = data[0x13];
+            Healing = new Heal(data[0x13]);
             Targeting = data[0x14];
             Stat1 = data[0x15];
             Stat2 = data[0x16];
@@ -347,6 +349,219 @@ namespace pk3DS
             _0x1F = data[0x1F];
             _0x20 = data[0x20];
             _0x21 = data[0x21];
+        }
+        public byte[] Write()
+        {
+            using (MemoryStream ms = new MemoryStream())
+            using (BinaryWriter bw = new BinaryWriter(ms))
+            {
+                bw.Write(Type);
+                bw.Write(Quality);
+                bw.Write(Category);
+                bw.Write(Power);
+                bw.Write(Accuracy);
+                bw.Write(PP);
+                bw.Write(Priority);
+                bw.Write((byte)(HitMin | (HitMax << 4)));
+                bw.Write(Inflict);
+                bw.Write(InflictPercent);
+                bw.Write(_0xB);
+                bw.Write(TurnMin);
+                bw.Write(TurnMax);
+                bw.Write(CritStage);
+                bw.Write(Flinch);
+                bw.Write(Effect);
+                bw.Write(Recoil);
+                bw.Write(Healing.Write());
+                bw.Write(Targeting);
+                bw.Write(Stat1);
+                bw.Write(Stat2);
+                bw.Write(Stat3);
+                bw.Write(Stat1Stage);
+                bw.Write(Stat2Stage);
+                bw.Write(Stat3Stage);
+                bw.Write(Stat1Percent);
+                bw.Write(Stat2Percent);
+                bw.Write(Stat3Percent);
+                bw.Write(_0x1E);
+                bw.Write(_0x1F);
+                bw.Write(_0x20);
+                bw.Write(_0x21);
+                return ms.ToArray();
+            }
+        }
+        public class Heal
+        {
+            public byte Val;
+            public bool Full, Half, Quarter, Value;
+            public Heal(byte val)
+            {
+                Val = val;
+                Full = Val == 0xFF;
+                Half = Val == 0xFE;
+                Quarter = Val == 0xFD;
+                Value = (Val < 0xFD);
+            }
+            public byte Write()
+            {
+                if (Value)
+                    return Val;
+                if (Full)
+                    return 0xFF;
+                if (Half)
+                    return 0xFE;
+                if (Quarter)
+                    return 0xFD;
+                return Val;
+            }
+        }
+    }
+    public class Item
+    {
+        private ushort Price;
+        public int BuyPrice;
+        public int SellPrice;
+        public byte HeldEffect, HeldArgument, NaturalGiftEffect, FlingEffect, FlingPower, NaturalGiftPower;
+        public byte NaturalGiftType, u8Flags;
+        public byte KeyFlags;
+        public byte UseEffect;
+        public byte _0xB; // Battle Type
+        public byte _0xC; // 0 or 1
+        public byte _0xD; // Classification (0-3 Battle, 4 Balls, 5 Mail)
+        public byte Consumable;
+        public byte SortIndex;
+        public byte CureInflict; // Bitflags
+        public byte FieldEffect; // Revive 1, Sacred Ash 3, Rare Candy 5, EvoStone 8
+        public int BoostATK, 
+            BoostDEF, BoostSPA, 
+            BoostSPD, BoostSPE, 
+            BoostACC, BoostCRIT, BoostPP;
+
+        public ushort FieldBoost; // 0x15-0x16
+        public byte EVHP, EVATK, EVDEF, EVSPE, EVSPA, EVSPD;
+        public Heal Healing;
+        public byte PPGain, Friendship1, Friendship2, Friendship3;
+
+        public Item(byte[] data)
+        {
+            using (BinaryReader br = new BinaryReader(new MemoryStream(data)))
+            {
+                Price = br.ReadUInt16();
+                BuyPrice = Price*10;
+                SellPrice = Price*5;
+
+                HeldEffect = br.ReadByte();
+                HeldArgument = br.ReadByte();
+                NaturalGiftEffect = br.ReadByte();
+                FlingEffect = br.ReadByte();
+                FlingPower = br.ReadByte();
+                NaturalGiftPower = br.ReadByte();
+                NaturalGiftType = br.ReadByte();
+                u8Flags = (byte)(NaturalGiftType >> 5);
+                NaturalGiftType &= 0x1F;
+                KeyFlags = br.ReadByte();
+                UseEffect = br.ReadByte();
+                _0xB = br.ReadByte();
+                _0xC = br.ReadByte();
+                _0xD = br.ReadByte();
+                Consumable = br.ReadByte();
+                SortIndex = br.ReadByte();
+                CureInflict = br.ReadByte();
+
+                FieldEffect = br.ReadByte();
+                BoostATK = FieldEffect >> 4;
+                FieldEffect &= 0xF;
+                BoostDEF = br.ReadByte();
+                BoostSPA = BoostDEF >> 4;
+                BoostDEF &= 0xF;
+                BoostSPD = br.ReadByte();
+                BoostSPE = BoostSPD >> 4;
+                BoostSPD &= 0xF;
+                BoostACC = br.ReadByte();
+                BoostCRIT = (BoostACC >> 4) & 0x3;
+                BoostPP = (BoostACC >> 6);
+                BoostACC &= 0xF;
+
+                EVHP = br.ReadByte();
+                EVATK = br.ReadByte();
+                EVDEF = br.ReadByte();
+                EVSPE = br.ReadByte();
+                EVSPA = br.ReadByte();
+                EVSPD = br.ReadByte();
+
+                Healing = new Heal(br.ReadByte());
+                PPGain = br.ReadByte();
+                Friendship1 = br.ReadByte();
+                Friendship2 = br.ReadByte();
+                Friendship3 = br.ReadByte();
+            }
+        }
+        public byte[] Write()
+        {
+            using (MemoryStream ms = new MemoryStream())
+            using (BinaryWriter bw = new BinaryWriter(ms))
+            {
+                bw.Write(Price);
+                bw.Write(HeldEffect);
+                bw.Write(HeldArgument);
+                bw.Write(NaturalGiftEffect);
+                bw.Write(FlingEffect);
+                bw.Write(FlingPower);
+                bw.Write(NaturalGiftPower);
+                bw.Write((byte)(NaturalGiftType | (u8Flags << 5)));
+                bw.Write(KeyFlags);
+                bw.Write(UseEffect);
+                bw.Write(_0xB);
+                bw.Write(_0xC);
+                bw.Write(_0xD);
+                bw.Write(Consumable);
+                bw.Write(SortIndex);
+                bw.Write(CureInflict);
+
+                bw.Write((byte)(FieldEffect | (BoostATK << 4)));
+                bw.Write((byte)(BoostDEF | (BoostSPA << 4)));
+                bw.Write((byte)(BoostSPD | (BoostSPE << 4)));
+                bw.Write((byte)(BoostACC | (BoostCRIT << 4) | (BoostPP << 6)));
+
+                bw.Write(EVHP);
+                bw.Write(EVATK);
+                bw.Write(EVDEF);
+                bw.Write(EVSPE);
+                bw.Write(EVSPA);
+                bw.Write(EVSPD);
+                bw.Write(Healing.Write());
+                bw.Write(PPGain);
+                bw.Write(Friendship1);
+                bw.Write(Friendship2);
+                bw.Write(Friendship3);
+
+                return ms.ToArray();
+            }
+        }
+        public class Heal
+        {
+            public byte Val;
+            public bool Full, Half, Quarter, Value;
+            public Heal(byte val)
+            {
+                Val = val;
+                Full = Val == 0xFF;
+                Half = Val == 0xFE;
+                Quarter = Val == 0xFD;
+                Value = (Val < 0xFD);
+            }
+            public byte Write()
+            {
+                if (Value)
+                    return Val;
+                if (Full)
+                    return 0xFF;
+                if (Half)
+                    return 0xFE;
+                if (Quarter)
+                    return 0xFD;
+                return Val;
+            }
         }
     }
     public class Maison
