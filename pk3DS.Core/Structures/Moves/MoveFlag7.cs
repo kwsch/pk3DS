8b817382d867b023f887c5fefacf9233e3231a30
Kurt <kaphotics@gmail.com>
2018-08-26 21:21:41 -0700

    Update flag naming / add docs
    #352
    comment descriptions from showdown (except AnimateAlly)

--- pk3DS.Core/Structures/Moves/MoveFlag7.cs
+++ pk3DS.Core/Structures/Moves/MoveFlag7.cs
@@ -7,25 +7,25 @@ namespace pk3DS.Core.Structures
     {
         None,
 
-        MakesContact     = 1u << 00,
-        Charge           = 1u << 01,
-        Recharge         = 1u << 02,
-        Protect          = 1u << 03,
-        Reflectable      = 1u << 04,
-        Snatch           = 1u << 05,
-        Mirror           = 1u << 06,
-        Punch            = 1u << 07,
+        MakesContact     = 1u << 00, // Makes contact.
+        Charge           = 1u << 01, // The user is unable to make a move between turns.
+        Recharge         = 1u << 02, // If this move is successful, the user must recharge on the following turn and cannot make a move.
+        Protect          = 1u << 03, // Blocked by Detect, Protect, Spiky Shield, and if not a Status move, King's Shield.
+        Reflectable      = 1u << 04, // Bounced back to the original user by Magic Coat or the Magic Bounce Ability.
+        Snatch           = 1u << 05, // Can be stolen from the original user and instead used by another Pokemon using Snatch.
+        Mirror           = 1u << 06, // Can be copied by Mirror Move.
+        Punch            = 1u << 07, // Power is multiplied when used by a Pokemon with the Iron Fist Ability.
 
-        Sound            = 1u << 08,
-        Gravity          = 1u << 09,
-        Defrost          = 1u << 10,
-        Distance         = 1u << 11,
-        Heal             = 1u << 12,
-        IgnoreSubstitute = 1u << 13,
-        NonSky           = 1u << 14,
-        Unknown          = 1u << 15,
+        Sound            = 1u << 08, // Has no effect on Pokemon with the Soundproof Ability.
+        Gravity          = 1u << 09, // Prevented from being executed or selected during Gravity's effect.
+        Defrost          = 1u << 10, // Thaws the user if executed successfully while the user is frozen.
+        DistanceTriple   = 1u << 11, // Can target a Pokemon positioned anywhere in a Triple Battle.
+        Heal             = 1u << 12, // Prevented from being executed or selected during Heal Block's effect.
+        IgnoreSubstitute = 1u << 13, // Ignores a target's substitute.
+        FailSkyBattle    = 1u << 14, // Prevented from being executed or selected in a Sky Battle.
+        AnimateAlly      = 1u << 15, // Always animate the move when used on an ally.
 
-        Dance            = 1u << 16,
+        Dance            = 1u << 16, // When used by a Pokemon, other Pokemon with the Dancer Ability can attempt to execute the same move.
         F18              = 1u << 17,
         F19              = 1u << 18,
         F20              = 1u << 19,
