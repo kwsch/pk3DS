ee169dcfbbcaeb03025af7c288a46f5a9dcfa08e
Kurt <kaphotics@gmail.com>
2019-01-02 23:45:14 -0800

    Split Consumable byte to 4&4
    flutes appear as '16', which is 0x10 (4th bit) flag.

--- pk3DS.Core/Structures/Gen6/Item6.cs
+++ pk3DS.Core/Structures/Gen6/Item6.cs
@@ -35,7 +35,7 @@ namespace pk3DS.Core.Structures
 
         public byte _0xC { get; set; } // 0 or 1
         public byte _0xD { get; set; } // Classification (0-3 Battle, 4 Balls, 5 Mail)
-        public byte Consumable { get; set; }
+        private byte Consumable { get; set; } // 4 bits for use consume, 4 bits for use not consumed
         public byte SortIndex { get; set; }
         public BattleStatusFlags CureInflict { get; set; } // Bitflags
         private byte Boost0; // Revive 1, Sacred Ash 3, Rare Candy 5, EvoStone 8, upper4 for BoostAtk
@@ -132,6 +132,12 @@ namespace pk3DS.Core.Structures
             get => (int) HealAmount;
             set => HealAmount = (HealValue)value;
         }
+
+        [Category(Heal), Description("Item is consumed when used."), RefreshProperties(RefreshProperties.All)]
+        public bool UseConsume { get => (Consumable & 0xF) != 0; set => Consumable = (byte)((Consumable & 0xF0) | (value ? 1 : 0)); }
+
+        [Category(Heal), Description("Item is not consumed when used."), RefreshProperties(RefreshProperties.All)]
+        public bool UseKeep { get => (Consumable & 0xF0) != 0; set => Consumable = (byte)((Consumable & 0x0F) | (value ? 0x10 : 0)); }
     }
 
     [Flags]
