b6c58cf5940e8cad34c0f6fe7be8cd3f9a8e9936
Koopa <codingkoopa@gmail.com>
2020-09-16 18:52:45 -0400

    Revert unintentional changes

--- pk3DS/Subforms/Gen7/StaticEncounterEditor7.cs
+++ pk3DS/Subforms/Gen7/StaticEncounterEditor7.cs
@@ -1,4 +1,4 @@
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Windows.Forms;
@@ -722,8 +722,6 @@ namespace pk3DS
             GetTrade();
 
             WinFormsUtil.Alert("Randomized Static Encounters according to specification!");
-
-            B_Starters_Click(sender, e);
         }
 
         // Mirror Changes
