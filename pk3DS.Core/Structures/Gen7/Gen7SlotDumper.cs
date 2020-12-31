0f329c0c8271ff64a8c735d4cfb838bcffb6a4f5
Matt <17801814+sora10pls@users.noreply.github.com>
2020-12-30 21:37:07 -0500

    Add Akala Outskirts to SM Inaccessible Tables

--- pk3DS.Core/Structures/Gen7/Gen7SlotDumper.cs
+++ pk3DS.Core/Structures/Gen7/Gen7SlotDumper.cs
@@ -229,7 +229,10 @@ namespace pk3DS.Core
             {074, new[] {1, 2, 3, 4}},
 
             // Memorial Hill
-            {083, new[] {1, 2}},
+            {083, new[] {3, 4, 5, 6, 7, 8, 9}},
+
+            // Akala Outskirts
+            {084, new[] {1, 2}},
 
             // Secluded Shore
             {142, new[] {1, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15}},
